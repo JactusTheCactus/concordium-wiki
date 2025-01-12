623f674871b9c8f75b17c33cbc74c3fb6bafc4dd
Devin <threateninglyamusing@gmail.com>
2025-01-11 19:46:09 -0800

    -

--- script.js
+++ script.js
@@ -60,12 +60,19 @@ fetch('concordium.json')
         Gear Colour: ${character.colour}<br>
         Power: ${character.power}<br>
         Species: ${character.species}<br>
-        Strength: ${character.sasia.str}<br>
-        Arcane: ${character.sasia.arc}<br>
-        Speed: ${character.sasia.spd}<br>
-        Intelligence: ${character.sasia.int}<br>
-        Alignment: ${character.sasia.aln} (${alignment})
       `;
+      if (character.epithet != `""`) {
+        stats.innerHTML += `
+        "${character.epithet}"
+      `
+      }
+      stats.innerHTML += `
+         Strength: ${character.sasia.str}<br>
+         Arcane: ${character.sasia.arc}<br>
+         Speed: ${character.sasia.spd}<br>
+         Intelligence: ${character.sasia.int}<br>
+         Alignment: ${character.sasia.aln} (${alignment})
+       `;
 
       // Append everything to the character container
       characterContainer.appendChild(name);
