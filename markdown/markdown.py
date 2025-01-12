ebe3471368d60d3da9ad4d13aac2b627ce38d285
Devin <threateninglyamusing@gmail.com>
2025-01-12 01:31:34 -0800

    -

--- markdown/markdown.py
+++ markdown/markdown.py
@@ -1,20 +1,33 @@
 import json
 with open('./concordium.json', 'r') as f:
     data = json.load(f)
+def ifNone(item,label):
+        if item != "":
+            if label != "Description":
+                item = f"""
+- **{label}**: {item}"""
+            else: item = f"""
+
+**{label}**:
+{item}"""
+        return item
 markdown = ""
+sin_ = input("Sins? Y/n ")
+virtue_ = input("Virtues? Y/n ")
 for key, value in data.items():
     name = value.get("name", "")
     animal = value.get("animal", "")
     aspect = value.get("aspect", "")
-    weapon = value.get("weapon", "")
-    colour = value.get("colour", "")
-    power = value.get("power", "")
-    species = value.get("species", "")
+    weapon = ifNone(value.get("weapon", ""),"Weapon")
+    colour = ifNone(value.get("colour", ""),"Colour")
+    power = ifNone(value.get("power", ""),"Power")
+    species = ifNone(value.get("species", ""),"Species")
     sex = value.get("sex", "")
     rank = value.get("rank", "")
     alignment = value.get("alignment", "")
-    inverse = value.get("inverse", "")
-    epithet = value.get("epithet", "")
+    epithet = ifNone(value.get("epithet", ""),"Epithet")
+    description = ifNone(value.get("description", ""),"Description")
+
     def gender(rank, sex):
         rank_map = {
             "Imperatore": {"F": "Imperatora", "M": "Imperator"},
@@ -28,23 +41,17 @@ for key, value in data.items():
         if rank in rank_map:
             return rank_map[rank].get(sex, rank)
         return rank
-    def ifNone(item,label):
-        if item != "":
-            item = f"""
-- **{label}**: {item}"""
-        return item
     title = f"""
 
-# {name} {gender(rank,sex)}, {animal} {alignment} of {aspect}
-"""
+# {name} {gender(rank,sex)}, {animal} {alignment} of {aspect}"""
     output = title
-    output += ifNone(epithet,"Epithet")
-    output += ifNone(weapon,"Weapon")
-    output += ifNone(colour,"Gear Colour")
-    output += ifNone(power,"Power")
-    output += ifNone(species,"Species")
-    output += ifNone(sex,"Sex")
-    output += ifNone(inverse,"Inverse")
-    markdown += output
+    output += epithet
+    output += weapon
+    output += colour
+    output += power
+    output += species
+    output += description
+    if (sin_ != "n" and alignment == "Sin") or (virtue_ != "n" and alignment == "Virtue"):
+        markdown += output
 with open('markdown/concordium.md', 'w') as md_file:
     md_file.write(markdown)
