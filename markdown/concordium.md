34d5184ae7a47a69e697c52481dcaf4be50554d6
Devin <threateninglyamusing@gmail.com>
2025-01-12 19:01:58 -0800

    -

--- markdown/concordium.md
+++ markdown/concordium.md
@@ -1,57 +1,45 @@
-
 # __The Concordium__
-
 __The Concordium__ was a group of 14 _Holy Knights_, directly serving the King. 7 of these Holy Knights betrayed the others, framing them for _high treason against the kingdom_, and sentencing them for a millenium. with this sentencing, the betrayed Holy Knights, now called __The Seven Deadly Sins__, were further punished with __The Capital Curses__, from _The Blood of The Dead Gods_. The other half of __The Concordium__ called themselves __The Seven Heavenly Virtues__, and continued selling their false tale of the Sins' betrayal.
-
 ## The Seven Deadly Sins
-
-
 ### Invidia Imperatora
-- **Curse**: The Curse of the Blue Gorgon
-- **Sin**: Envy
-- **Weapon**: Staff
-- **Power**: Mimicry
-- **Species**: Changeling
-
+- __Curse__: The Curse of the Blue Gorgon
+- __Sin__: Envy
+- __Weapon__: Staff
+- __Power__: Mimicry
+- __Species__: Changeling
 ### Gula Venator
-- **Curse**: The Curse of the Purple Hydra
-- **Sin**: Gluttony
-- **Weapon**: Battleaxe
-- **Power**: Power Absorption
-
+- __Curse__: The Curse of the Purple Hydra
+- __Sin__: Gluttony
+- __Weapon__: Battleaxe
+- __Power__: Power Absorption
 ### Avaritia Ferrator
-- **Curse**: The Curse of the Green Dragon
-- **Sin**: Greed
-- **Weapon**: Warhammer
-- **Power**: Ferrokinesis
-
+- __Curse__: The Curse of the Green Dragon
+- __Sin__: Greed
+- __Weapon__: Warhammer
+- __Power__: Ferrokinesis
 ### Luxuria Domina
-- **Curse**: The Curse of the Pink Siren
-- **Sin**: Lust
-- **Weapon**: Daggers
-- **Power**: Illusion
-- **Species**: Faerie
-
+- __Curse__: The Curse of the Pink Siren
+- __Sin__: Lust
+- __Weapon__: Daggers
+- __Power__: Illusion
+- __Species__: Faerie
 ### Superbia Luminora
-- **Curse**: The Curse of the Yellow Phoenix
-- **Sin**: Pride
-- **Weapon**: Sword
-- **Power**: Resurrective Self-Immolation
-- **Species**: Elf
-
+- __Curse__: The Curse of the Yellow Phoenix
+- __Sin__: Pride
+- __Weapon__: Sword
+- __Power__: Resurrective Self-Immolation
+- __Species__: Elf
 ### Acedia Exaltus
-- **Curse**: The Curse of the Orange Sphinx
-- **Sin**: Sloth
-- **Weapon**: Mace
-- **Power**: Paralysis
-- **Species**: Golem
-
+- __Curse__: The Curse of the Orange Sphinx
+- __Sin__: Sloth
+- __Weapon__: Mace
+- __Power__: Paralysis
+- __Species__: Golem
 ### Ira Bellatrix
-- **Epithet**: The Chainbreaker
-- **Curse**: The Curse of the Red Kraken
-- **Sin**: Wrath
-- **Weapon**: Chain-Whip
-- **Power**: Strength
-
-**Description**:
-Growing up under the boot of all those more fortunate than her, Ira developed a strong sense of justice and an intense hatred for injustice and abuse of power. After being sentenced as the Sin of Wrath for a thousand years, then being cursed with The Curse of The Kraken upon her release, she sought to find every abuser of power and reduce them to little more than a crater in the ground.
+- __Epithet__: The Chainbreaker
+- __Curse__: The Curse of the Red Kraken
+- __Sin__: Wrath
+- __Weapon__: Chain-Whip
+- __Power__: Strength
+- __Description__:
+    - Growing up under the boot of all those more fortunate than her, Ira developed a strong sense of justice and an intense hatred for injustice and abuse of power. After being sentenced as the Sin of Wrath for a thousand years, then being cursed with The Curse of The Kraken upon her release, she sought to find every abuser of power and reduce them to little more than a crater in the ground.
--- markdown/markdown.py
+++ markdown/markdown.py
@@ -2,47 +2,48 @@ import json
 with open('./concordium.json', 'r') as f:
     data = json.load(f)
 def ifNone(item,label):
-        if item != "":
-            if label != "Description":
-                item = f"""
-- **{label}**: {item}"""
-            else: item = f"""
-
-**{label}**:
-{item}"""
-        return item
+    global alignment
+    if item != "":
+        newLabel = f"__{label}__"
+        if label == "Description" or label == "Species Definition":
+            out = f"""- {newLabel}:
+    - {item}
+"""
+        else:
+            out = f"""- {newLabel}: {item}
+"""
+    else: out = ""
+    return out
 markdown = ""
 README = ""
 firstSin = True
-sinTitle = """
-## The Seven Deadly Sins
+sinTitle = """## The Seven Deadly Sins
 """
 firstVirtue = True
-virtueTitle = """
-## The Seven Heavenly Virtues
+virtueTitle = """## The Seven Heavenly Virtues
 """
-docTitle = f"""
-# __The Concordium__
-
+docTitle = f"""# __The Concordium__
 __The Concordium__ was a group of 14 _Holy Knights_, directly serving the King. 7 of these Holy Knights betrayed the others, framing them for _high treason against the kingdom_, and sentencing them for a millenium. with this sentencing, the betrayed Holy Knights, now called __The Seven Deadly Sins__, were further punished with __The Capital Curses__, from _The Blood of The Dead Gods_. The other half of __The Concordium__ called themselves __The Seven Heavenly Virtues__, and continued selling their false tale of the Sins' betrayal.
 """
-sin_ = "Y"
-virtue_ = "n"
+sin_ = True
+virtue_ = False
 for key, value in data.items():
     name = value.get("name", "")
     animal = value.get("animal", "")
     aspect = value.get("aspect", "")
-    weapon = ifNone(value.get("weapon", ""),"Weapon")
+    ifWeapon = ifNone(value.get("weapon", ""),"Weapon")
     colour = value.get("colour", "")
-    power = ifNone(value.get("power", ""),"Power")
-    species = ifNone(value.get("species", ""),"Species")
+    ifPower = ifNone(value.get("power", ""),"Power")
+    species = value.get("species","")
+    ifSpecies = ifNone(species,"Species")
+    speciesDef = ifNone(value.get("speciesDef", ""), "Species Definition")
     sex = value.get("sex", "")
     rank = value.get("rank", "")
     alignment = value.get("alignment", "")
     if alignment == "Sin": mark = "Curse"
     elif alignment == "Virtue": mark = "Blessing"
-    epithet = ifNone(value.get("epithet", ""),"Epithet")
-    description = ifNone(value.get("description", ""),"Description")
+    ifEpithet = ifNone(value.get("epithet", ""),"Epithet")
+    ifDescription = ifNone(value.get("description", ""),"Description")
 
     def gender(rank, sex):
         rank_map = {
@@ -57,38 +58,38 @@ for key, value in data.items():
         if rank in rank_map:
             return rank_map[rank].get(sex, rank)
         return rank
-    title = f"""
-
-### {name} {gender(rank,sex)}"""
+    title = f"""### {name} {gender(rank,sex)}
+"""
     output = title
-    output += epithet
+    output += ifEpithet
     output += ifNone(f"The {mark} of the {colour} {animal}",mark)
     output += ifNone(aspect,alignment)
-    output += weapon
-    output += power
-    output += species
-    output += description
+    output += ifWeapon
+    output += ifPower
+    output += ifSpecies
+    output += speciesDef
+    output += ifDescription
     if firstSin and firstVirtue:
         markdown += docTitle
         README += docTitle
     if alignment == "Sin":
         if firstSin:
-            if sin_ != "n".lower():
+            if sin_:
                 markdown += sinTitle
             README += sinTitle
             firstSin = False
-        if sin_ != "n".lower():
+        if sin_:
             markdown += output
     if alignment == "Virtue":
         if firstVirtue:
-            if virtue_ != "n".lower():
+            if virtue_:
                 markdown += virtueTitle
             README += virtueTitle
             firstVirtue = False
-        if virtue_ != "n".lower():
+        if virtue_:
             markdown += output
     README += output
-with open('markdown/concordium.md', 'w') as md_file:
-    md_file.write(markdown)
-with open('README.md', 'w') as md_file:
-    md_file.write(README)
+    with open('markdown/concordium.md', 'w') as md_file:
+        md_file.write(markdown)
+    with open('README.md', 'w') as md_file:
+        md_file.write(README)
