668baf682ad5eeaf9209deb386ac990c219a639a
JactusTheCactus <threateninglyamusing@gmail.com>
2025-01-16 22:24:46 -0800

    Update list.js

--- character-cards/list.js
+++ character-cards/list.js
@@ -6,7 +6,56 @@ fetch('data.json')
 
     Object.values(data).forEach(character => {
       const div = document.createElement('div');
-      div.innerHTML = `${character.name} (${character.animal})`;
+      if (character.alignment === "Sin") {team = `The Seven Deadly Sins`}
+      else if (character.alignment === "Virtue") {team = `The Seven Heavenly Virtues`}
+      if (character.rank === "Imperatore") {job = `, Captain of <u>${team}</u>`}
+      else {job = ""}
+      function sex(character) {
+        const rankMap = {
+          "Imperatore": { "F": "Imperatora", "M": "Imperator" },
+          "Venatorium": { "F": "Venatrix", "M": "Venator"},
+          "Ferratorium": { "F": "Ferratrix", "M": "Ferrator"},
+          "Dominum": { "F": "Domina", "M": "Dominus"},
+          "Luminorium": { "F": "Luminora", "M": "Luminor"},
+          "Exaltum": { "F": "Exalta", "M": "Exaltus"},
+          "Bellatorium": { "F": "Bellatrix", "M": "Bellator"},
+        };
+        if (rankMap[character.rank]) {return rankMap[character.rank][character.sex] || character.rank;}
+        return character.rank;
+      }
+      div.innerHTML = `
+        <h3>${character.name} ${sex(character)}, ${character.animal} ${character.alignment} of ${character.aspect}${job}</h3>
+      `;
+      if (character.epithet !== "") {
+      div.innerHTML += `
+        <p><b>"${character.epithet}"</b></p>
+      `;
+      }
+      if (character.weapon !== "") {
+      div.innerHTML += `
+        <li>Weapon: <u>${character.weapon}</u></li>
+      `;
+      }
+      if (character.colour !== "") {
+      div.innerHTML += `
+        <li>Gear Colour: <u>${character.colour}</u></li>
+      `;
+      }
+      if (character.power !== "") {
+      div.innerHTML += `
+        <li>Power: <u>${character.power}</u></li>
+      `;
+      }
+      if (character.species !== "") {
+      div.innerHTML += `
+        <li>Species: <u>${character.species}</u></li>
+      `;
+      }
+      if (character.description !== "") {
+      div.innerHTML += `
+        <p>${character.description}</p>
+      `;
+      }
 
       if (character.alignment === "Sin") {
         sinsList.appendChild(div);
