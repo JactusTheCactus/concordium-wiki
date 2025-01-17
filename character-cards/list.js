3eb0810608f00d0fd420bc02c02bb86f8ead23c0
JactusTheCactus <threateninglyamusing@gmail.com>
2025-01-16 22:23:38 -0800

    Update list.js

--- character-cards/list.js
+++ character-cards/list.js
@@ -3,13 +3,15 @@ fetch('data.json')
   .then(data => {
     const sinsList = document.getElementById('sins-list');
     const virtuesList = document.getElementById('virtues-list');
+
     Object.values(data).forEach(character => {
-      const listItem = document.createElement('li');
-      listItem.innerHTML = `${character.name} (${character.animal})`;
+      const div = document.createElement('div');
+      div.innerHTML = `${character.name} (${character.animal})`;
+
       if (character.alignment === "Sin") {
-        sinsList.appendChild(listItem);
+        sinsList.appendChild(div);
       } else if (character.alignment === "Virtue") {
-        virtuesList.appendChild(listItem);
+        virtuesList.appendChild(div);
       }
     });
   })
